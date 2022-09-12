64b25889d68cd3bd5f4ab758bf8d547982b9daac
unknown <thuliopv360@gmail.com>
2022-09-12 16:45:32 -0300

    feature: creation the category crud and working

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/categories/dto/create-category.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Nome da categoria',
+    example: 'Nft',
+  })
+  name: string;
+}

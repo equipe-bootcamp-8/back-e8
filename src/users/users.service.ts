5f3d30a87285a782167c5b81a9a927268a7cf27c
KleitonLima <kleiton.mini@gmail.com>
2022-09-12 23:14:37 -0300

    feat(users.service): constructor and return findAll

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(dto: CreateUserDto): User {
     return;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
   findOne(id: string) {

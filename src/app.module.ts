a4a70ccbebaa2297a035ce455dc113282f715386
KleitonLima <kleiton.mini@gmail.com>
2022-09-12 21:51:06 -0300

    feat(app.module) add UsersModule in imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })

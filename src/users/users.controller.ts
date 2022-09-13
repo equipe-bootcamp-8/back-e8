434addfa3defd2d9b861f85a20122986fe4b972b
KleitonLima <kleiton.mini@gmail.com>
2022-09-12 22:51:36 -0300

    feat(users.controller): create documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,33 +11,50 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Create user',
+  })
   create(@Body() dto: CreateUserDto): User {
     return this.usersService.create(dto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'List users',
+  })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'List user by id',
+  })
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Update user',
+  })
   update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
-    return this.usersService.update(+id, dto);
+    return this.usersService.update(id, dto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete user',
+  })
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.remove(id);
   }
 }

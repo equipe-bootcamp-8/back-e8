f53bf54182eed0f0b9b077a824b8b5a9f2d31803
KleitonLima <kleiton.mini@gmail.com>
2022-09-21 20:00:41 -0300

    feat(jwt.strategy): created and configured

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,31 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: { email: string }) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: payload.email },
+    });
+
+    if (!user) {
+      throw new NotFoundException(
+        'User does not exist or is not authenticated',
+      );
+    }
+
+    delete user.password;
+
+    return user;
+  }
+}

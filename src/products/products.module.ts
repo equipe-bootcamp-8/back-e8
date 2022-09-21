58e4133f8515784f6bd329d32c55d9314b91c8c1
KleitonLima <kleiton.mini@gmail.com>
2022-09-21 20:44:33 -0300

    feat(modules): add PassportModule on imports...
    registering defaultStrategy from jwt

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })

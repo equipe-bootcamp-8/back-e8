45a78885f8ff2ac02eb5ee0a7189c9dc1856dd0b
KleitonLima <kleiton.mini@gmail.com>
2022-09-21 22:00:59 -0300

    feat(main): alternative port to deploy

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
 }
 bootstrap();

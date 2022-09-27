e248ae4ef608c13489c12bc4c151e5ff1d1f8177
KleitonLima <kleiton.mini@gmail.com>
2022-09-26 22:04:22 -0300

    fix(create-user): corrected regex...
    removed MinLength to password, Matches message includes more information

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  Matches,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
@@ -23,10 +17,13 @@ export class CreateUserDto {
   })
   email: string;
 
-  @MinLength(8)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'Weak password',
-  })
+  @Matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/,
+    {
+      message:
+        'Weak password! \n It must contain at least 8 characters, \n being an uppercase letter, \n a lowercase letter, \n a number and one of the special characters: $*&@#',
+    },
+  )
   @ApiProperty({
     description: 'Password user',
     example: 'Abc1234@',

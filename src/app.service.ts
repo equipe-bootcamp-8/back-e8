54df3f2714a1c5d9e71474fa6eb1cbe2db26a510
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-11-11 19:31:05 -0300

    feature: link swagger

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getAppStatus(): string {
-    return 'Server is running! 🚀\n Please check http://localhost:3333/docs for Swagger docs...';
+    return 'Server is running! 🚀\n Please check https://cloudwalk-backend.onrender.com/docs for Swagger docs...';
   }
 }
